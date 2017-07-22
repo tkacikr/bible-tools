93dbdbf90e33e2d774e540ea4fe18f52486a1e35
VitalikL <lim.vitaliy@gmail.com>
2017-07-21 21:41:13 -0400

    Updating zh with synonyms

--- bibles/zh/cuvs/info.js
+++ bibles/zh/cuvs/info.js
@@ -1,469 +1,601 @@
 var info = {
-	"lang": "zh",
-	"version": "cuvs",
-	"books": [
-		{
-			"name": "創 世 記",
-			"numChapters": 50,
-			"synonyms": [
-				"創 世 記"
-			]
-		},
-		{
-			"name": "出 埃 及 記",
-			"numChapters": 40,
-			"synonyms": [
-				"出 埃 及 記"
-			]
-		},
-		{
-			"name": "利 未 記",
-			"numChapters": 27,
-			"synonyms": [
-				"利 未 記"
-			]
-		},
-		{
-			"name": "民 數 記",
-			"numChapters": 36,
-			"synonyms": [
-				"民 數 記"
-			]
-		},
-		{
-			"name": "申 命 記",
-			"numChapters": 34,
-			"synonyms": [
-				"申 命 記"
-			]
-		},
-		{
-			"name": "約 書 亞 記",
-			"numChapters": 24,
-			"synonyms": [
-				"約 書 亞 記"
-			]
-		},
-		{
-			"name": "士 師 記",
-			"numChapters": 21,
-			"synonyms": [
-				"士 師 記"
-			]
-		},
-		{
-			"name": "路 得 記",
-			"numChapters": 4,
-			"synonyms": [
-				"路 得 記"
-			]
-		},
-		{
-			"name": "撒 母 耳 記 上",
-			"numChapters": 31,
-			"synonyms": [
-				"撒 母 耳 記 上"
-			]
-		},
-		{
-			"name": "撒 母 耳 記 下",
-			"numChapters": 24,
-			"synonyms": [
-				"撒 母 耳 記 下"
-			]
-		},
-		{
-			"name": "列 王 紀 上",
-			"numChapters": 22,
-			"synonyms": [
-				"列 王 紀 上"
-			]
-		},
-		{
-			"name": "列 王 紀 下",
-			"numChapters": 25,
-			"synonyms": [
-				"列 王 紀 下"
-			]
-		},
-		{
-			"name": "歷 代 志 上",
-			"numChapters": 29,
-			"synonyms": [
-				"歷 代 志 上"
-			]
-		},
-		{
-			"name": "歷 代 志 下",
-			"numChapters": 36,
-			"synonyms": [
-				"歷 代 志 下"
-			]
-		},
-		{
-			"name": "以 斯 拉 記",
-			"numChapters": 10,
-			"synonyms": [
-				"以 斯 拉 記"
-			]
-		},
-		{
-			"name": "尼 希 米 記",
-			"numChapters": 13,
-			"synonyms": [
-				"尼 希 米 記"
-			]
-		},
-		{
-			"name": "以 斯 帖 記",
-			"numChapters": 10,
-			"synonyms": [
-				"以 斯 帖 記"
-			]
-		},
-		{
-			"name": "約 伯 記",
-			"numChapters": 42,
-			"synonyms": [
-				"約 伯 記"
-			]
-		},
-		{
-			"name": "詩 篇",
-			"numChapters": 150,
-			"synonyms": [
-				"詩 篇"
-			]
-		},
-		{
-			"name": "箴 言",
-			"numChapters": 31,
-			"synonyms": [
-				"箴 言"
-			]
-		},
-		{
-			"name": "傳 道 書",
-			"numChapters": 12,
-			"synonyms": [
-				"傳 道 書"
-			]
-		},
-		{
-			"name": "雅 歌",
-			"numChapters": 8,
-			"synonyms": [
-				"雅 歌"
-			]
-		},
-		{
-			"name": "以 賽 亞 書",
-			"numChapters": 66,
-			"synonyms": [
-				"以 賽 亞 書"
-			]
-		},
-		{
-			"name": "耶 利 米 書",
-			"numChapters": 52,
-			"synonyms": [
-				"耶 利 米 書"
-			]
-		},
-		{
-			"name": "耶 利 米 哀 歌",
-			"numChapters": 5,
-			"synonyms": [
-				"耶 利 米 哀 歌"
-			]
-		},
-		{
-			"name": "以 西 結 書",
-			"numChapters": 48,
-			"synonyms": [
-				"以 西 結 書"
-			]
-		},
-		{
-			"name": "但 以 理 書",
-			"numChapters": 12,
-			"synonyms": [
-				"但 以 理 書"
-			]
-		},
-		{
-			"name": "何 西 阿 書",
-			"numChapters": 14,
-			"synonyms": [
-				"何 西 阿 書"
-			]
-		},
-		{
-			"name": "約 珥 書",
-			"numChapters": 3,
-			"synonyms": [
-				"約 珥 書"
-			]
-		},
-		{
-			"name": "阿 摩 司 書",
-			"numChapters": 9,
-			"synonyms": [
-				"阿 摩 司 書"
-			]
-		},
-		{
-			"name": "俄 巴 底 亞 書",
-			"numChapters": 1,
-			"synonyms": [
-				"俄 巴 底 亞 書"
-			]
-		},
-		{
-			"name": "約 拿 書",
-			"numChapters": 4,
-			"synonyms": [
-				"約 拿 書"
-			]
-		},
-		{
-			"name": "彌 迦 書",
-			"numChapters": 7,
-			"synonyms": [
-				"彌 迦 書"
-			]
-		},
-		{
-			"name": "那 鴻 書",
-			"numChapters": 3,
-			"synonyms": [
-				"那 鴻 書"
-			]
-		},
-		{
-			"name": "哈 巴 谷 書",
-			"numChapters": 3,
-			"synonyms": [
-				"哈 巴 谷 書"
-			]
-		},
-		{
-			"name": "西 番 雅 書",
-			"numChapters": 3,
-			"synonyms": [
-				"西 番 雅 書"
-			]
-		},
-		{
-			"name": "哈 該 書",
-			"numChapters": 2,
-			"synonyms": [
-				"哈 該 書"
-			]
-		},
-		{
-			"name": "撒 迦 利 亞",
-			"numChapters": 14,
-			"synonyms": [
-				"撒 迦 利 亞"
-			]
-		},
-		{
-			"name": "瑪 拉 基 書",
-			"numChapters": 4,
-			"synonyms": [
-				"瑪 拉 基 書"
-			]
-		},
-		{
-			"name": "馬 太 福 音",
-			"numChapters": 28,
-			"synonyms": [
-				"馬 太 福 音"
-			]
-		},
-		{
-			"name": "馬 可 福 音",
-			"numChapters": 16,
-			"synonyms": [
-				"馬 可 福 音"
-			]
-		},
-		{
-			"name": "路 加 福 音",
-			"numChapters": 24,
-			"synonyms": [
-				"路 加 福 音"
-			]
-		},
-		{
-			"name": "約 翰 福 音",
-			"numChapters": 21,
-			"synonyms": [
-				"約 翰 福 音"
-			]
-		},
-		{
-			"name": "使 徒 行 傳",
-			"numChapters": 28,
-			"synonyms": [
-				"使 徒 行 傳"
-			]
-		},
-		{
-			"name": "羅 馬 書",
-			"numChapters": 16,
-			"synonyms": [
-				"羅 馬 書"
-			]
-		},
-		{
-			"name": "歌 林 多 前 書",
-			"numChapters": 16,
-			"synonyms": [
-				"歌 林 多 前 書"
-			]
-		},
-		{
-			"name": "歌 林 多 後 書",
-			"numChapters": 13,
-			"synonyms": [
-				"歌 林 多 後 書"
-			]
-		},
-		{
-			"name": "加 拉 太 書",
-			"numChapters": 6,
-			"synonyms": [
-				"加 拉 太 書"
-			]
-		},
-		{
-			"name": "以 弗 所 書",
-			"numChapters": 6,
-			"synonyms": [
-				"以 弗 所 書"
-			]
-		},
-		{
-			"name": "腓 立 比 書",
-			"numChapters": 4,
-			"synonyms": [
-				"腓 立 比 書"
-			]
-		},
-		{
-			"name": "歌 羅 西 書",
-			"numChapters": 4,
-			"synonyms": [
-				"歌 羅 西 書"
-			]
-		},
-		{
-			"name": "帖 撒 羅 尼 迦 前 書",
-			"numChapters": 5,
-			"synonyms": [
-				"帖 撒 羅 尼 迦 前 書"
-			]
-		},
-		{
-			"name": "帖 撒 羅 尼 迦 後 書",
-			"numChapters": 3,
-			"synonyms": [
-				"帖 撒 羅 尼 迦 後 書"
-			]
-		},
-		{
-			"name": "提 摩 太 前 書",
-			"numChapters": 6,
-			"synonyms": [
-				"提 摩 太 前 書"
-			]
-		},
-		{
-			"name": "提 摩 太 後 書",
-			"numChapters": 4,
-			"synonyms": [
-				"提 摩 太 後 書"
-			]
-		},
-		{
-			"name": "提 多 書",
-			"numChapters": 3,
-			"synonyms": [
-				"提 多 書"
-			]
-		},
-		{
-			"name": "腓 利 門 書",
-			"numChapters": 1,
-			"synonyms": [
-				"腓 利 門 書"
-			]
-		},
-		{
-			"name": "希 伯 來 書",
-			"numChapters": 13,
-			"synonyms": [
-				"希 伯 來 書"
-			]
-		},
-		{
-			"name": "雅 各 書",
-			"numChapters": 5,
-			"synonyms": [
-				"雅 各 書"
-			]
-		},
-		{
-			"name": "彼 得 前 書",
-			"numChapters": 5,
-			"synonyms": [
-				"彼 得 前 書"
-			]
-		},
-		{
-			"name": "彼 得 後 書",
-			"numChapters": 3,
-			"synonyms": [
-				"彼 得 後 書"
-			]
-		},
-		{
-			"name": "約 翰 一 書",
-			"numChapters": 5,
-			"synonyms": [
-				"約 翰 一 書"
-			]
-		},
-		{
-			"name": "約 翰 二 書",
-			"numChapters": 1,
-			"synonyms": [
-				"約 翰 二 書"
-			]
-		},
-		{
-			"name": "約 翰 三 書",
-			"numChapters": 1,
-			"synonyms": [
-				"約 翰 三 書"
-			]
-		},
-		{
-			"name": "猶 大 書",
-			"numChapters": 1,
-			"synonyms": [
-				"猶 大 書"
-			]
-		},
-		{
-			"name": "启 示 录",
-			"numChapters": 22,
-			"synonyms": [
-				"启 示 录"
-			]
-		}
-	]
+    "lang": "zh",
+    "version": "cuvs",
+    "books": [
+        {
+            "name": "創 世 記",
+            "numChapters": 50,
+            "synonyms": [
+                "創 世 記",
+                "创世记",
+                "创"
+            ]
+        },
+        {
+            "name": "出 埃 及 記",
+            "numChapters": 40,
+            "synonyms": [
+                "出 埃 及 記",
+                "出埃及记",
+                "出"
+            ]
+        },
+        {
+            "name": "利 未 記",
+            "numChapters": 27,
+            "synonyms": [
+                "利 未 記",
+                "利未记",
+                "利"
+            ]
+        },
+        {
+            "name": "民 數 記",
+            "numChapters": 36,
+            "synonyms": [
+                "民 數 記",
+                "民数记",
+                "民"
+            ]
+        },
+        {
+            "name": "申 命 記",
+            "numChapters": 34,
+            "synonyms": [
+                "申 命 記",
+                "申命记",
+                "申"
+            ]
+        },
+        {
+            "name": "約 書 亞 記",
+            "numChapters": 24,
+            "synonyms": [
+                "約 書 亞 記",
+                "约书亚记",
+                "书"
+            ]
+        },
+        {
+            "name": "士 師 記",
+            "numChapters": 21,
+            "synonyms": [
+                "士 師 記",
+                "士师记",
+                "士"
+            ]
+        },
+        {
+            "name": "路 得 記",
+            "numChapters": 4,
+            "synonyms": [
+                "路 得 記",
+                "路得记",
+                "得"
+            ]
+        },
+        {
+            "name": "撒 母 耳 記 上",
+            "numChapters": 31,
+            "synonyms": [
+                "撒 母 耳 記 上",
+                "撒母耳记上",
+                "撒上"
+            ]
+        },
+        {
+            "name": "撒 母 耳 記 下",
+            "numChapters": 24,
+            "synonyms": [
+                "撒 母 耳 記 下",
+                "撒母耳记下",
+                "撒下"
+            ]
+        },
+        {
+            "name": "列 王 紀 上",
+            "numChapters": 22,
+            "synonyms": [
+                "列 王 紀 上",
+                "列王纪上",
+                "王上"
+            ]
+        },
+        {
+            "name": "列 王 紀 下",
+            "numChapters": 25,
+            "synonyms": [
+                "列 王 紀 下",
+                "列王纪下",
+                "王下"
+            ]
+        },
+        {
+            "name": "歷 代 志 上",
+            "numChapters": 29,
+            "synonyms": [
+                "歷 代 志 上",
+                "历代志上",
+                "代上"
+            ]
+        },
+        {
+            "name": "歷 代 志 下",
+            "numChapters": 36,
+            "synonyms": [
+                "歷 代 志 下",
+                "历代志下",
+                "代下"
+            ]
+        },
+        {
+            "name": "以 斯 拉 記",
+            "numChapters": 10,
+            "synonyms": [
+                "以 斯 拉 記",
+                "以斯拉记",
+                "拉"
+            ]
+        },
+        {
+            "name": "尼 希 米 記",
+            "numChapters": 13,
+            "synonyms": [
+                "尼 希 米 記",
+                "尼希米记",
+                "尼"
+            ]
+        },
+        {
+            "name": "以 斯 帖 記",
+            "numChapters": 10,
+            "synonyms": [
+                "以 斯 帖 記",
+                "以斯帖记",
+                "斯"
+            ]
+        },
+        {
+            "name": "約 伯 記",
+            "numChapters": 42,
+            "synonyms": [
+                "約 伯 記",
+                "约伯记",
+                "伯"
+            ]
+        },
+        {
+            "name": "詩 篇",
+            "numChapters": 150,
+            "synonyms": [
+                "詩 篇",
+                "诗篇",
+                "诗"
+            ]
+        },
+        {
+            "name": "箴 言",
+            "numChapters": 31,
+            "synonyms": [
+                "箴 言",
+                "箴言",
+                "箴"
+            ]
+        },
+        {
+            "name": "傳 道 書",
+            "numChapters": 12,
+            "synonyms": [
+                "傳 道 書",
+                "传道书",
+                "传"
+            ]
+        },
+        {
+            "name": "雅 歌",
+            "numChapters": 8,
+            "synonyms": [
+                "雅 歌",
+                "雅歌",
+                "歌"
+            ]
+        },
+        {
+            "name": "以 賽 亞 書",
+            "numChapters": 66,
+            "synonyms": [
+                "以 賽 亞 書",
+                "以赛亚书",
+                "赛"
+            ]
+        },
+        {
+            "name": "耶 利 米 書",
+            "numChapters": 52,
+            "synonyms": [
+                "耶 利 米 書",
+                "耶利米书",
+                "耶"
+            ]
+        },
+        {
+            "name": "耶 利 米 哀 歌",
+            "numChapters": 5,
+            "synonyms": [
+                "耶 利 米 哀 歌",
+                "耶利米哀歌",
+                "哀"
+            ]
+        },
+        {
+            "name": "以 西 結 書",
+            "numChapters": 48,
+            "synonyms": [
+                "以 西 結 書",
+                "以西结书",
+                "结"
+            ]
+        },
+        {
+            "name": "但 以 理 書",
+            "numChapters": 12,
+            "synonyms": [
+                "但 以 理 書",
+                "但以理书",
+                "但"
+            ]
+        },
+        {
+            "name": "何 西 阿 書",
+            "numChapters": 14,
+            "synonyms": [
+                "何 西 阿 書",
+                "何西阿书",
+                "何"
+            ]
+        },
+        {
+            "name": "約 珥 書",
+            "numChapters": 3,
+            "synonyms": [
+                "約 珥 書",
+                "约珥书",
+                "珥"
+            ]
+        },
+        {
+            "name": "阿 摩 司 書",
+            "numChapters": 9,
+            "synonyms": [
+                "阿 摩 司 書",
+                "阿摩司书",
+                "摩"
+            ]
+        },
+        {
+            "name": "俄 巴 底 亞 書",
+            "numChapters": 1,
+            "synonyms": [
+                "俄 巴 底 亞 書",
+                "俄巴底亚书",
+                "俄"
+            ]
+        },
+        {
+            "name": "約 拿 書",
+            "numChapters": 4,
+            "synonyms": [
+                "約 拿 書",
+                "约拿书",
+                "拿"
+            ]
+        },
+        {
+            "name": "彌 迦 書",
+            "numChapters": 7,
+            "synonyms": [
+                "彌 迦 書",
+                "弥迦书",
+                "弥"
+            ]
+        },
+        {
+            "name": "那 鴻 書",
+            "numChapters": 3,
+            "synonyms": [
+                "那 鴻 書",
+                "那鸿书",
+                "鸿"
+            ]
+        },
+        {
+            "name": "哈 巴 谷 書",
+            "numChapters": 3,
+            "synonyms": [
+                "哈 巴 谷 書",
+                "哈巴谷书",
+                "哈"
+            ]
+        },
+        {
+            "name": "西 番 雅 書",
+            "numChapters": 3,
+            "synonyms": [
+                "西 番 雅 書",
+                "西番雅书",
+                "番"
+            ]
+        },
+        {
+            "name": "哈 該 書",
+            "numChapters": 2,
+            "synonyms": [
+                "哈 該 書",
+                "哈该书",
+                "该"
+            ]
+        },
+        {
+            "name": "撒 迦 利 亞",
+            "numChapters": 14,
+            "synonyms": [
+                "撒 迦 利 亞",
+                "撒迦利亚书",
+                "亚"
+            ]
+        },
+        {
+            "name": "瑪 拉 基 書",
+            "numChapters": 4,
+            "synonyms": [
+                "瑪 拉 基 書",
+                "玛拉基书",
+                "玛"
+            ]
+        },
+        {
+            "name": "馬 太 福 音",
+            "numChapters": 28,
+            "synonyms": [
+                "馬 太 福 音",
+                "马太福音",
+                "太"
+            ]
+        },
+        {
+            "name": "馬 可 福 音",
+            "numChapters": 16,
+            "synonyms": [
+                "馬 可 福 音",
+                "马可福音",
+                "可"
+            ]
+        },
+        {
+            "name": "路 加 福 音",
+            "numChapters": 24,
+            "synonyms": [
+                "路 加 福 音",
+                "路加福音",
+                "路"
+            ]
+        },
+        {
+            "name": "約 翰 福 音",
+            "numChapters": 21,
+            "synonyms": [
+                "約 翰 福 音",
+                "约翰福音",
+                "约"
+            ]
+        },
+        {
+            "name": "使 徒 行 傳",
+            "numChapters": 28,
+            "synonyms": [
+                "使 徒 行 傳",
+                "使徒行传",
+                "徒"
+            ]
+        },
+        {
+            "name": "羅 馬 書",
+            "numChapters": 16,
+            "synonyms": [
+                "羅 馬 書",
+                "罗马书",
+                "罗"
+            ]
+        },
+        {
+            "name": "歌 林 多 前 書",
+            "numChapters": 16,
+            "synonyms": [
+                "歌 林 多 前 書",
+                "哥林多前书",
+                "林前"
+            ]
+        },
+        {
+            "name": "歌 林 多 後 書",
+            "numChapters": 13,
+            "synonyms": [
+                "歌 林 多 後 書",
+                "哥林多后书",
+                "林后"
+            ]
+        },
+        {
+            "name": "加 拉 太 書",
+            "numChapters": 6,
+            "synonyms": [
+                "加 拉 太 書",
+                "加拉太书",
+                "加"
+            ]
+        },
+        {
+            "name": "以 弗 所 書",
+            "numChapters": 6,
+            "synonyms": [
+                "以 弗 所 書",
+                "以弗所书",
+                "弗"
+            ]
+        },
+        {
+            "name": "腓 立 比 書",
+            "numChapters": 4,
+            "synonyms": [
+                "腓 立 比 書",
+                "腓立比书",
+                "腓"
+            ]
+        },
+        {
+            "name": "歌 羅 西 書",
+            "numChapters": 4,
+            "synonyms": [
+                "歌 羅 西 書",
+                "歌罗西书",
+                "西"
+            ]
+        },
+        {
+            "name": "帖 撒 羅 尼 迦 前 書",
+            "numChapters": 5,
+            "synonyms": [
+                "帖 撒 羅 尼 迦 前 書",
+                "帖撒罗尼迦前书",
+                "帖前"
+            ]
+        },
+        {
+            "name": "帖 撒 羅 尼 迦 後 書",
+            "numChapters": 3,
+            "synonyms": [
+                "帖 撒 羅 尼 迦 後 書",
+                "帖撒罗尼迦后书",
+                "帖后"
+            ]
+        },
+        {
+            "name": "提 摩 太 前 書",
+            "numChapters": 6,
+            "synonyms": [
+                "提 摩 太 前 書",
+                "提摩太前书",
+                "提前"
+            ]
+        },
+        {
+            "name": "提 摩 太 後 書",
+            "numChapters": 4,
+            "synonyms": [
+                "提 摩 太 後 書",
+                "提摩太后书",
+                "提后"
+            ]
+        },
+        {
+            "name": "提 多 書",
+            "numChapters": 3,
+            "synonyms": [
+                "提 多 書",
+                "提多书",
+                "多"
+            ]
+        },
+        {
+            "name": "腓 利 門 書",
+            "numChapters": 1,
+            "synonyms": [
+                "腓 利 門 書",
+                "腓利门书",
+                "门"
+            ]
+        },
+        {
+            "name": "希 伯 來 書",
+            "numChapters": 13,
+            "synonyms": [
+                "希 伯 來 書",
+                "希伯来书",
+                "来"
+            ]
+        },
+        {
+            "name": "雅 各 書",
+            "numChapters": 5,
+            "synonyms": [
+                "雅 各 書",
+                "雅各书",
+                "雅"
+            ]
+        },
+        {
+            "name": "彼 得 前 書",
+            "numChapters": 5,
+            "synonyms": [
+                "彼 得 前 書",
+                "彼得前书",
+                "彼前"
+            ]
+        },
+        {
+            "name": "彼 得 後 書",
+            "numChapters": 3,
+            "synonyms": [
+                "彼 得 後 書",
+                "彼得后书",
+                "彼后"
+            ]
+        },
+        {
+            "name": "約 翰 一 書",
+            "numChapters": 5,
+            "synonyms": [
+                "約 翰 一 書",
+                "约翰一书",
+                "约一"
+            ]
+        },
+        {
+            "name": "約 翰 二 書",
+            "numChapters": 1,
+            "synonyms": [
+                "約 翰 二 書",
+                "约翰二书",
+                "约二"
+            ]
+        },
+        {
+            "name": "約 翰 三 書",
+            "numChapters": 1,
+            "synonyms": [
+                "約 翰 三 書",
+                "约翰三书",
+                "约三"
+            ]
+        },
+        {
+            "name": "猶 大 書",
+            "numChapters": 1,
+            "synonyms": [
+                "猶 大 書",
+                "犹太书",
+                "犹"
+            ]
+        },
+        {
+            "name": "启 示 录",
+            "numChapters": 22,
+            "synonyms": [
+                "启 示 录",
+                "启示录",
+                "启"
+            ]
+        }
+    ]
 };
 module.exports = info;
