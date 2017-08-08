05767b25aabd01955128955c26fed28e429b2dda
imasaru <evanmr4@gmail.com>
2017-08-08 14:30:25 -0500

    Added literal to ja

--- bibles/ja/literals.js
+++ bibles/ja/literals.js
@@ -1,9 +1,21 @@
 var literals = {
+<<<<<<< refs/remotes/Adventech/master
     "and": {
         "list": [
             "と"
         ]
     }
+=======
+    and: [
+        "と",
+    ],
+
+    through: [
+        "〜",
+        "~",
+        "~"
+    ]
+>>>>>>> Added literal to ja
 };
 
 module.exports = literals;
