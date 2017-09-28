3d5a42323a58b41f51856ccd4cfd98b1bbfc56da
VitalikL <lim.vitaliy@gmail.com>
2017-09-27 22:06:57 -0400

    Add Thessalonians synonym to de/luth1545 and de/luth1912

--- bibles/de/luth1545/info.js
+++ bibles/de/luth1545/info.js
@@ -373,6 +373,7 @@ var info = {
 			"numChapters": 16,
 			"synonyms": [
 				"Römer",
+				"Römer",
 				"Roemer",
 				"Roem",
 				"Röm"
@@ -428,14 +429,16 @@ var info = {
 			"name": "1 Thessalonicher",
 			"numChapters": 5,
 			"synonyms": [
-				"1\.? Thess(alonicher)?"
+				"1\.? Thess(alonicher)?",
+				"1\.? Ths"
 			]
 		},
 		{
 			"name": "2 Thessalonicher",
 			"numChapters": 3,
 			"synonyms": [
-				"2\.? Thess(alonicher)?"
+				"2\.? Thess(alonicher)?",
+                "2\.? Ths"
 			]
 		},
 		{
