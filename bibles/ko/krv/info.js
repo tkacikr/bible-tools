40c2539accbf1c74cce326397d061e7993bb7b00
VitalikL <lim.vitaliy@gmail.com>
2018-01-11 02:27:13 -0500

    Adding ko/krv Bible!

---
+++ bibles/ko/krv/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "ko",
+	"version": "krv",
+	"books": [
+		{
+			"name": "창세기",
+			"numChapters": 50,
+			"synonyms": [
+				"Gen"
+			]
+		},
+		{
+			"name": "출애굽기",
+			"numChapters": 40,
+			"synonyms": [
+				"Exod"
+			]
+		},
+		{
+			"name": "레위기",
+			"numChapters": 27,
+			"synonyms": [
+				"Lev"
+			]
+		},
+		{
+			"name": "민수기",
+			"numChapters": 36,
+			"synonyms": [
+				"Num"
+			]
+		},
+		{
+			"name": "신명기",
+			"numChapters": 34,
+			"synonyms": [
+				"Deut"
+			]
+		},
+		{
+			"name": "여호수아 ì",
+			"numChapters": 24,
+			"synonyms": [
+				"Josh"
+			]
+		},
+		{
+			"name": "사사기",
+			"numChapters": 21,
+			"synonyms": [
+				"Judg"
+			]
+		},
+		{
+			"name": "룻기",
+			"numChapters": 4,
+			"synonyms": [
+				"Ruth"
+			]
+		},
+		{
+			"name": "사무엘상",
+			"numChapters": 31,
+			"synonyms": [
+				"1Sam"
+			]
+		},
+		{
+			"name": "사무엘하",
+			"numChapters": 24,
+			"synonyms": [
+				"2Sam"
+			]
+		},
+		{
+			"name": "열왕기상",
+			"numChapters": 22,
+			"synonyms": [
+				"1Kgs"
+			]
+		},
+		{
+			"name": "열왕기하",
+			"numChapters": 25,
+			"synonyms": [
+				"2Kgs"
+			]
+		},
+		{
+			"name": "역대상",
+			"numChapters": 29,
+			"synonyms": [
+				"1Chr"
+			]
+		},
+		{
+			"name": "역대하",
+			"numChapters": 36,
+			"synonyms": [
+				"2Chr"
+			]
+		},
+		{
+			"name": "에스라",
+			"numChapters": 10,
+			"synonyms": [
+				"Ezra"
+			]
+		},
+		{
+			"name": "느헤미야",
+			"numChapters": 13,
+			"synonyms": [
+				"Neh"
+			]
+		},
+		{
+			"name": "에스더",
+			"numChapters": 10,
+			"synonyms": [
+				"Esth"
+			]
+		},
+		{
+			"name": "욥기",
+			"numChapters": 42,
+			"synonyms": [
+				"Job"
+			]
+		},
+		{
+			"name": "시편",
+			"numChapters": 150,
+			"synonyms": [
+				"Ps"
+			]
+		},
+		{
+			"name": "잠언",
+			"numChapters": 31,
+			"synonyms": [
+				"Prov"
+			]
+		},
+		{
+			"name": "전도서",
+			"numChapters": 12,
+			"synonyms": [
+				"Eccl"
+			]
+		},
+		{
+			"name": "아가",
+			"numChapters": 8,
+			"synonyms": [
+				"Song"
+			]
+		},
+		{
+			"name": "이사야",
+			"numChapters": 66,
+			"synonyms": [
+				"Isa"
+			]
+		},
+		{
+			"name": "예레미야",
+			"numChapters": 52,
+			"synonyms": [
+				"Jer"
+			]
+		},
+		{
+			"name": "예레미야 애가",
+			"numChapters": 5,
+			"synonyms": [
+				"Lam"
+			]
+		},
+		{
+			"name": "에스겔",
+			"numChapters": 48,
+			"synonyms": [
+				"Ezek"
+			]
+		},
+		{
+			"name": "다니엘",
+			"numChapters": 12,
+			"synonyms": [
+				"Dan"
+			]
+		},
+		{
+			"name": "호세아",
+			"numChapters": 14,
+			"synonyms": [
+				"Hos"
+			]
+		},
+		{
+			"name": "요엘",
+			"numChapters": 3,
+			"synonyms": [
+				"Joel"
+			]
+		},
+		{
+			"name": "아모스",
+			"numChapters": 9,
+			"synonyms": [
+				"Amos"
+			]
+		},
+		{
+			"name": "오바댜",
+			"numChapters": 1,
+			"synonyms": [
+				"Obad"
+			]
+		},
+		{
+			"name": "요나",
+			"numChapters": 4,
+			"synonyms": [
+				"Jonah"
+			]
+		},
+		{
+			"name": "미가",
+			"numChapters": 7,
+			"synonyms": [
+				"Mic"
+			]
+		},
+		{
+			"name": "나훔",
+			"numChapters": 3,
+			"synonyms": [
+				"Nah"
+			]
+		},
+		{
+			"name": "하박국",
+			"numChapters": 3,
+			"synonyms": [
+				"Hab"
+			]
+		},
+		{
+			"name": "스바냐",
+			"numChapters": 3,
+			"synonyms": [
+				"Zeph"
+			]
+		},
+		{
+			"name": "학개",
+			"numChapters": 2,
+			"synonyms": [
+				"Hag"
+			]
+		},
+		{
+			"name": "스가랴",
+			"numChapters": 14,
+			"synonyms": [
+				"Zech"
+			]
+		},
+		{
+			"name": "말라기",
+			"numChapters": 4,
+			"synonyms": [
+				"Mal"
+			]
+		},
+		{
+			"name": "마태복음",
+			"numChapters": 28,
+			"synonyms": [
+				"Matt"
+			]
+		},
+		{
+			"name": "마가복음",
+			"numChapters": 16,
+			"synonyms": [
+				"Mark"
+			]
+		},
+		{
+			"name": "누가복음",
+			"numChapters": 24,
+			"synonyms": [
+				"Luke"
+			]
+		},
+		{
+			"name": "요한복음",
+			"numChapters": 21,
+			"synonyms": [
+				"John"
+			]
+		},
+		{
+			"name": "사도행전",
+			"numChapters": 28,
+			"synonyms": [
+				"Acts"
+			]
+		},
+		{
+			"name": "로마서",
+			"numChapters": 16,
+			"synonyms": [
+				"Rom"
+			]
+		},
+		{
+			"name": "고린도전서",
+			"numChapters": 16,
+			"synonyms": [
+				"1Cor"
+			]
+		},
+		{
+			"name": "고린도후서",
+			"numChapters": 13,
+			"synonyms": [
+				"2Cor"
+			]
+		},
+		{
+			"name": "갈라디아서",
+			"numChapters": 6,
+			"synonyms": [
+				"Gal"
+			]
+		},
+		{
+			"name": "에베소서",
+			"numChapters": 6,
+			"synonyms": [
+				"Eph"
+			]
+		},
+		{
+			"name": "빌립보서",
+			"numChapters": 4,
+			"synonyms": [
+				"Phil"
+			]
+		},
+		{
+			"name": "골로새서",
+			"numChapters": 4,
+			"synonyms": [
+				"Col"
+			]
+		},
+		{
+			"name": "데살로니가전서",
+			"numChapters": 5,
+			"synonyms": [
+				"1Thess"
+			]
+		},
+		{
+			"name": "데살로니가후서",
+			"numChapters": 3,
+			"synonyms": [
+				"2Thess"
+			]
+		},
+		{
+			"name": "디모데전서",
+			"numChapters": 6,
+			"synonyms": [
+				"1Tim"
+			]
+		},
+		{
+			"name": "디모데후서",
+			"numChapters": 4,
+			"synonyms": [
+				"2Tim"
+			]
+		},
+		{
+			"name": "디도서",
+			"numChapters": 3,
+			"synonyms": [
+				"Titus"
+			]
+		},
+		{
+			"name": "빌레몬서",
+			"numChapters": 1,
+			"synonyms": [
+				"Phlm"
+			]
+		},
+		{
+			"name": "히브리서",
+			"numChapters": 13,
+			"synonyms": [
+				"Heb"
+			]
+		},
+		{
+			"name": "야고보서",
+			"numChapters": 5,
+			"synonyms": [
+				"Jas"
+			]
+		},
+		{
+			"name": "베드로전서",
+			"numChapters": 5,
+			"synonyms": [
+				"1Pet"
+			]
+		},
+		{
+			"name": "베드로후서",
+			"numChapters": 3,
+			"synonyms": [
+				"2Pet"
+			]
+		},
+		{
+			"name": "요한1서",
+			"numChapters": 5,
+			"synonyms": [
+				"1John"
+			]
+		},
+		{
+			"name": "요한2서",
+			"numChapters": 1,
+			"synonyms": [
+				"2John"
+			]
+		},
+		{
+			"name": "요한3서",
+			"numChapters": 1,
+			"synonyms": [
+				"3John"
+			]
+		},
+		{
+			"name": "유다서",
+			"numChapters": 1,
+			"synonyms": [
+				"Jude"
+			]
+		},
+		{
+			"name": "요한계시록",
+			"numChapters": 22,
+			"synonyms": [
+				"Rev"
+			]
+		}
+	]
+};
+module.exports = info;
