2c3d52cb927cb750e95ccb03f0f54e39149a1060
VitalikL <lim.vitaliy@gmail.com>
2018-04-12 00:19:31 -0400

    Adding th/th-1971

---
+++ bibles/th/th-1971/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "th",
+	"version": "th-1971",
+	"books": [
+		{
+			"name": "ปฐม‍กาล",
+			"numChapters": 50,
+			"synonyms": [
+				"Gen"
+			]
+		},
+		{
+			"name": "อพ‌ยพ",
+			"numChapters": 40,
+			"synonyms": [
+				"Exod"
+			]
+		},
+		{
+			"name": "เลวี‌นิติ",
+			"numChapters": 27,
+			"synonyms": [
+				"Lev"
+			]
+		},
+		{
+			"name": "กัน‌ดาร‍วิถี",
+			"numChapters": 36,
+			"synonyms": [
+				"Num"
+			]
+		},
+		{
+			"name": "เฉลย‍ธรรม‍บัญ‌ญัติ",
+			"numChapters": 34,
+			"synonyms": [
+				"Deut"
+			]
+		},
+		{
+			"name": "โย‌ชู‌วา",
+			"numChapters": 24,
+			"synonyms": [
+				"Josh"
+			]
+		},
+		{
+			"name": "ผู้‍วินิจ‌ฉัย",
+			"numChapters": 21,
+			"synonyms": [
+				"Judg"
+			]
+		},
+		{
+			"name": "นาง​รูธ",
+			"numChapters": 4,
+			"synonyms": [
+				"Ruth"
+			]
+		},
+		{
+			"name": "1 ซา‌มู‌เอล",
+			"numChapters": 31,
+			"synonyms": [
+				"1Sam"
+			]
+		},
+		{
+			"name": "2 ซา‌มู‌เอล",
+			"numChapters": 24,
+			"synonyms": [
+				"2Sam"
+			]
+		},
+		{
+			"name": "1 พงศ์‍กษัตริย์",
+			"numChapters": 22,
+			"synonyms": [
+				"1Kgs"
+			]
+		},
+		{
+			"name": "2 พงศ์‍กษัตริย์",
+			"numChapters": 25,
+			"synonyms": [
+				"2Kgs"
+			]
+		},
+		{
+			"name": "1 พง‌ศาว‌ดาร",
+			"numChapters": 29,
+			"synonyms": [
+				"1Chr"
+			]
+		},
+		{
+			"name": "2 พง‌ศาว‌ดาร",
+			"numChapters": 36,
+			"synonyms": [
+				"2Chr"
+			]
+		},
+		{
+			"name": "เอส‌รา",
+			"numChapters": 10,
+			"synonyms": [
+				"Ezra"
+			]
+		},
+		{
+			"name": "เนหะ‌มีย์",
+			"numChapters": 13,
+			"synonyms": [
+				"Neh"
+			]
+		},
+		{
+			"name": "เอส‌เธอร์",
+			"numChapters": 10,
+			"synonyms": [
+				"Esth"
+			]
+		},
+		{
+			"name": "โยบ",
+			"numChapters": 42,
+			"synonyms": [
+				"Job"
+			]
+		},
+		{
+			"name": "สดุดี",
+			"numChapters": 150,
+			"synonyms": [
+				"Ps"
+			]
+		},
+		{
+			"name": "สุภา‌ษิต",
+			"numChapters": 31,
+			"synonyms": [
+				"Prov"
+			]
+		},
+		{
+			"name": "ปัญ‌ญา‌จารย์",
+			"numChapters": 12,
+			"synonyms": [
+				"Eccl"
+			]
+		},
+		{
+			"name": "เพลง‍ซา‌โล‌มอน",
+			"numChapters": 8,
+			"synonyms": [
+				"Song"
+			]
+		},
+		{
+			"name": "อิส‌ยาห์",
+			"numChapters": 66,
+			"synonyms": [
+				"Isa"
+			]
+		},
+		{
+			"name": "เย‌เร‌มีย์",
+			"numChapters": 52,
+			"synonyms": [
+				"Jer"
+			]
+		},
+		{
+			"name": "บท‍เพลง​คร่ำ‍ครวญ",
+			"numChapters": 5,
+			"synonyms": [
+				"Lam"
+			]
+		},
+		{
+			"name": "เอ‌เส‌เคียล",
+			"numChapters": 48,
+			"synonyms": [
+				"Ezek"
+			]
+		},
+		{
+			"name": "ดา‌เนียล",
+			"numChapters": 12,
+			"synonyms": [
+				"Dan"
+			]
+		},
+		{
+			"name": "โฮ‌เช‌ยา",
+			"numChapters": 14,
+			"synonyms": [
+				"Hos"
+			]
+		},
+		{
+			"name": "โย‌เอล",
+			"numChapters": 3,
+			"synonyms": [
+				"Joel"
+			]
+		},
+		{
+			"name": "อา‌โมส",
+			"numChapters": 9,
+			"synonyms": [
+				"Amos"
+			]
+		},
+		{
+			"name": "โอบาดีห์",
+			"numChapters": 1,
+			"synonyms": [
+				"Obad"
+			]
+		},
+		{
+			"name": "โย‌นาห์",
+			"numChapters": 4,
+			"synonyms": [
+				"Jonah"
+			]
+		},
+		{
+			"name": "มี‌คาห์",
+			"numChapters": 7,
+			"synonyms": [
+				"Mic"
+			]
+		},
+		{
+			"name": "นา‌ฮูม",
+			"numChapters": 3,
+			"synonyms": [
+				"Nah"
+			]
+		},
+		{
+			"name": "ฮา‌บา‌กุก",
+			"numChapters": 3,
+			"synonyms": [
+				"Hab"
+			]
+		},
+		{
+			"name": "เศ‌ฟัน‌ยาห์",
+			"numChapters": 3,
+			"synonyms": [
+				"Zeph"
+			]
+		},
+		{
+			"name": "ฮัก‌กัย",
+			"numChapters": 2,
+			"synonyms": [
+				"Hag"
+			]
+		},
+		{
+			"name": "เศ‌คา‌ริ‌ยาห์",
+			"numChapters": 14,
+			"synonyms": [
+				"Zech"
+			]
+		},
+		{
+			"name": "มา‌ลา‌คี",
+			"numChapters": 4,
+			"synonyms": [
+				"Mal"
+			]
+		},
+		{
+			"name": "มัท‌ธิว",
+			"numChapters": 28,
+			"synonyms": [
+				"Matt"
+			]
+		},
+		{
+			"name": "มา‌ระ‌โก",
+			"numChapters": 16,
+			"synonyms": [
+				"Mark"
+			]
+		},
+		{
+			"name": "ลูกา",
+			"numChapters": 24,
+			"synonyms": [
+				"Luke"
+			]
+		},
+		{
+			"name": "ยอห์น",
+			"numChapters": 21,
+			"synonyms": [
+				"John"
+			]
+		},
+		{
+			"name": "กิจ‍การ​ของ​อัคร‌ทูต",
+			"numChapters": 28,
+			"synonyms": [
+				"Acts"
+			]
+		},
+		{
+			"name": "โรม",
+			"numChapters": 16,
+			"synonyms": [
+				"Rom"
+			]
+		},
+		{
+			"name": "1 โค‌รินธ์",
+			"numChapters": 16,
+			"synonyms": [
+				"1Cor"
+			]
+		},
+		{
+			"name": "2 โค‌รินธ์",
+			"numChapters": 13,
+			"synonyms": [
+				"2Cor"
+			]
+		},
+		{
+			"name": "กา‌ลา‌เทีย",
+			"numChapters": 6,
+			"synonyms": [
+				"Gal"
+			]
+		},
+		{
+			"name": "เอ‌เฟ‌ซัส",
+			"numChapters": 6,
+			"synonyms": [
+				"Eph"
+			]
+		},
+		{
+			"name": "ฟีลิปปี",
+			"numChapters": 4,
+			"synonyms": [
+				"Phil"
+			]
+		},
+		{
+			"name": "โค‌โล‌สี",
+			"numChapters": 4,
+			"synonyms": [
+				"Col"
+			]
+		},
+		{
+			"name": "1 เธสะ‌โล‌นิ‌กา",
+			"numChapters": 5,
+			"synonyms": [
+				"1Thess"
+			]
+		},
+		{
+			"name": "2 เธสะ‌โล‌นิ‌กา",
+			"numChapters": 3,
+			"synonyms": [
+				"2Thess"
+			]
+		},
+		{
+			"name": "1 ทิ‌โม‌ธี",
+			"numChapters": 6,
+			"synonyms": [
+				"1Tim"
+			]
+		},
+		{
+			"name": "2 ทิ‌โม‌ธี",
+			"numChapters": 4,
+			"synonyms": [
+				"2Tim"
+			]
+		},
+		{
+			"name": "ทิตัส",
+			"numChapters": 3,
+			"synonyms": [
+				"Titus"
+			]
+		},
+		{
+			"name": "ฟีเลโมน",
+			"numChapters": 1,
+			"synonyms": [
+				"Phlm"
+			]
+		},
+		{
+			"name": "ฮีบรู",
+			"numChapters": 13,
+			"synonyms": [
+				"Heb"
+			]
+		},
+		{
+			"name": "ยา‌กอบ",
+			"numChapters": 5,
+			"synonyms": [
+				"Jas"
+			]
+		},
+		{
+			"name": "1 เป‌โตร",
+			"numChapters": 5,
+			"synonyms": [
+				"1Pet"
+			]
+		},
+		{
+			"name": "2 เป‌โตร",
+			"numChapters": 3,
+			"synonyms": [
+				"2Pet"
+			]
+		},
+		{
+			"name": "1 ยอห์น",
+			"numChapters": 5,
+			"synonyms": [
+				"1John"
+			]
+		},
+		{
+			"name": "2 ยอห์น",
+			"numChapters": 1,
+			"synonyms": [
+				"2John"
+			]
+		},
+		{
+			"name": "3 ยอห์น",
+			"numChapters": 1,
+			"synonyms": [
+				"3John"
+			]
+		},
+		{
+			"name": "ยูดา",
+			"numChapters": 1,
+			"synonyms": [
+				"Jude"
+			]
+		},
+		{
+			"name": "วิวรณ์",
+			"numChapters": 22,
+			"synonyms": [
+				"Rev"
+			]
+		}
+	]
+};
+module.exports = info;
